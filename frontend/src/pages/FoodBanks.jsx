6f10e5a812f57f36104e5d97e1e9f8c3eab2772f
“AnupamSistla” <sistlaanupam@gmail.com>
2025-10-22 23:07:15 -0500

    Made changes to upgrade the UI of the dashboard

--- frontend/src/pages/FoodBanks.jsx
+++ frontend/src/pages/FoodBanks.jsx
@@ -1,10 +1,13 @@
-import "./pages.css";
+import "./FoodBanks.css";
 
 function FoodBanks() 
 {
   return (
     <div id="foodbanks">
-
+      <h1>Food Banks Directory</h1>
+      <div className="foodbanks-content">
+        <p>Directory of verified food banks and their current needs will be displayed here.</p>
+      </div>
     
     </div>
   )
